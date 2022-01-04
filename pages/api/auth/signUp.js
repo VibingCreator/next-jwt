cbd797fd6e791f578fc74a4ad34e29e45951b8de
Radosław Miluk <VibingCreator.Workspace@gmail.com>
2022-01-04 02:23:20 +0100

    store user data in database

--- pages/api/auth/signUp.js
+++ pages/api/auth/signUp.js
@@ -1,4 +1,7 @@
-const handler = (req, res) => {
+import { hash } from "../../../lib/crypto";
+import prisma from "../../../lib/prisma";
+
+const handler = async (req, res) => {
     // !TODO: properly validate data
     if (req.method !== "POST") {
         return res.status(405).json({ message: "Method Not Allowed" });
@@ -10,7 +13,19 @@ const handler = (req, res) => {
         return res.status(400).json({ message: "Bad Request" });
     }
 
-    return res.status(201).json(body);
+    const username = body.username.trim().toLowerCase();
+    const email = body.email.trim().toLowerCase();
+    const password = hash(body.password);
+
+    try {
+        await prisma.user.create({
+            data: { username, email, password }
+        });
+    } catch (err) {
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+
+    return res.status(201).json({ message: "Created" });
 };
 
 export { handler as default };
